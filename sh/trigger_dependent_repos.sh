861f8263d00536abf48248ed011098f42c327764
Jon Jagger <jon@jaggersoft.com>
2019-02-20 10:44:03 +0000

    remove debug statements from script; problem was .gitignore in the web repo

--- sh/trigger_dependent_repos.sh
+++ sh/trigger_dependent_repos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex
+set -ev
 
 declare -r SHA="${1}"
 declare -r REPO_URLS="${@:2}"
@@ -10,9 +10,7 @@ for REPO_URL in ${REPO_URLS}; do                   # eg https://github.com/cyber
   git clone "${REPO_URL}"
   cd "${REPO_NAME}"
   echo "${SHA}" > web-base.trigger
-  git status
   git add .
-  git status
   git config --global user.email "cyber-dojo-machine-user@cyber-dojo.org"
   git config --global user.name "Machine User"
   git commit -m "automated build trigger from cyberdojo/web-base ${SHA}"
