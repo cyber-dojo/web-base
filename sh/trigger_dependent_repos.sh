d44d9df269de2f5cf1767d442af2df88e99e9514
Jon Jagger <jon@jaggersoft.com>
2019-06-02 08:59:43 +0100

    improve message in automated git commit trigger of dependent repos

--- sh/trigger_dependent_repos.sh
+++ sh/trigger_dependent_repos.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 set -ev
 
-declare -r SHA="${1}"
-declare -r REPO_URLS="${@:2}"
+declare -r FROM="${1}"          # eg web-base
+declare -r SHA="${2}"           # eg a9334c964f81800a910dc3d301543262161fbbff
+declare -r REPO_NAMES="${@:3}"  # eg saver ragger ...
 
-for REPO_URL in ${REPO_URLS}; do                   # eg https://github.com/cyber-dojo/differ.git
-  declare REPO_NAME=$(basename "${REPO_URL}" .git) # eg differ
+declare -r COMMIT_URL="https://github.com/cyber-dojo/${FROM}/commit/${SHA}"
+
+for REPO_NAME in ${REPO_NAMES}; do
+  declare REPO_URL="https://github.com/cyber-dojo/${REPO_NAME}.git"
   cd /tmp
   git clone "${REPO_URL}"
   cd "${REPO_NAME}"
-  echo "${SHA}" > web-base.trigger
+  declare MESSAGE="automated build trigger from ${COMMIT_URL}}"
+  echo "${MESSAGE}" > ${FROM}.trigger
   git add .
   git config --global user.email "cyber-dojo-machine-user@cyber-dojo.org"
   git config --global user.name "Machine User"
-  git commit -m "automated build trigger from cyberdojo/web-base ${SHA}"
+  git commit -m "${MESSAGE}"
   git push origin master
 done
