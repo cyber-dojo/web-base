009815754a8da10c323720e53b4691f5cda02a36
Jon Jagger <jon@jaggersoft.com>
2020-10-08 20:00:21 +0100

    Do non-anonymous DockerHub pulls on CI (anonymous pulls have stricter pull limits)

--- build_test_publish.sh
+++ build_test_publish.sh
@@ -5,4 +5,3 @@ readonly SH_DIR="$( cd "$( dirname "${0}" )/sh" && pwd )"
 "${SH_DIR}/build_docker_images.sh"
 "${SH_DIR}/tag_image.sh"
 "${SH_DIR}/on_ci_publish_tagged_images.sh"
-#TODO: trigger dependent repos
--- sh/on_ci_publish_tagged_images.sh
+++ sh/on_ci_publish_tagged_images.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -Ee
+#!/bin/bash -Eeu
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
 on_ci_publish_tagged_images()
@@ -11,17 +11,14 @@ on_ci_publish_tagged_images()
   local -r image="$(image_name)"
   local -r sha="$(image_sha)"
   local -r tag=${sha:0:7}
-  # DOCKER_USER, DOCKER_PASS are in ci context
-  echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
   docker push ${image}:latest
   docker push ${image}:${tag}
-  docker logout
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
 on_ci()
 {
-  [ -n "${CIRCLECI}" ]
+  [ -n "${CIRCLECI:-}" ]
 }
 
 #- - - - - - - - - - - - - - - - - - - - - - - -
