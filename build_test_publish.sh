1d61007fb15af771b6f9e51006cce15f79f41e78
JonJagger <jon@kosli.com>
2025-02-12 09:12:52 +0000

    Fix script name

--- build_test_publish.sh
+++ build_test_publish.sh
@@ -2,6 +2,6 @@
 
 readonly SH_DIR="$( cd "$( dirname "${0}" )/sh" && pwd )"
 
-"${SH_DIR}/build_docker_images.sh"
+"${SH_DIR}/build_docker_image.sh"
 "${SH_DIR}/tag_image.sh"
 "${SH_DIR}/on_ci_publish_tagged_images.sh"
